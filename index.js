4ea04ffcc19babfc7dd5a2d39268cf1769226ceb
Lian <biakhong@gmail.com>
2022-05-26 01:34:10 -0400

    Second Submittion

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+
+
 let direction = document.getElementById("direct")
 let value = document.getElementById("textValue")
 let submitBt = document.getElementById("enter")
@@ -5,9 +9,18 @@ let form =document.querySelector('form');
 let theSentences = document.getElementById("theSentence")
 let forms = document.getElementById("forms")
 let button = document.getElementById("btns")
+let likes = document.getElementById("urLikes")
+let likeList = document.getElementById("likeList")
 
 
-let directionWords = ["Enter an adjective:", "Enter an verb:", "Enter another adjective:", "Enter another noun:", "Enter a person name:", "Enter any food name:"]
+let btnInner = `<button class="btn" id="btn"> Play Again </button>`
+let likeInner =`<button id="likeBtn"> Like </button>`
+
+
+let wordForLike;
+let imgForLike;
+
+let directionWords = ["Enter an adjective:", "Enter an verb:", "Enter another adjective:", "Enter a noun:", "Enter a person name:", "Enter any food name:",""]
 let theObject={
 		"adjective": "",
 		"verb": "",
@@ -22,15 +35,16 @@ function handleSubmit() {
         let tvalue = e.target.textValue.value;
         direction.innerHTML = changeWord(direction.innerHTML , directionWords, tvalue)
         console.log(theObject);
-        if(direction.innerHTML === directionWords[5]){
+        if(direction.innerHTML === directionWords[6]){
             postObj(theObject)
         let mytext =`A ${theObject.adjective} man ${theObject.verb} into the ${theObject.adjective2} building to visit a very nice ${theObject.noun}.
          Sit down Mr/Mrs ${theObject.name} can I interest you with any good ${theObject.food}.`
+         wordForLike = mytext;
         console.log(mytext)
             randerSentence(mytext);
             renderCats();
             button.innerHTML=btnInner;
-            
+            likes.innerHTML=likeInner;            
            
             
             theObject={
@@ -39,7 +53,6 @@ function handleSubmit() {
         "adjective2":"",
 		"noun": "",
         "name": "",
-		"verb2": "",
 		"food": ""
         }
         }
@@ -51,6 +64,7 @@ function handleSubmit() {
     })
 }
 handleSubmit()
+
 function changeWord(curWord, wordsArr, value){
     
     switch (curWord) {
@@ -63,19 +77,22 @@ function changeWord(curWord, wordsArr, value){
             return curWord = wordsArr[2];
           break;
         case wordsArr[2]:
-            theObject.place=value;
+            theObject.adjective2=value;
             return curWord = wordsArr[3];
           break;
           case wordsArr[3]:
-            theObject.verb2=value;
+            theObject.noun=value;
             return curWord = wordsArr[4];
           break;
           case wordsArr[4]:
-            theObject.noun=value;
+            theObject.name=value;
             return curWord=wordsArr[5];
           break;
           case wordsArr[5]:
-            theObject.verb=value;
+            theObject.food=value;
+            return curWord = wordsArr[6];
+          break;
+          case wordsArr[6]:
             return curWord = wordsArr[0];
           break;
       }
@@ -87,18 +104,40 @@ function getAllData(){
         console.log("Hallelujah")))
 }
 
+
+
+likes.addEventListener("click", handleLikes)
+
+function handleLikes(){
+
+    const newDiv = document.createElement("div");
+    let something = `<li>
+    <a>
+      <img src=${imgForLike} />
+    </a>
+    <div class="content">
+      <p>${wordForLike}</p>
+    </div>
+  </li>`
+  newDiv.innerHTML=(something)
+  likeList.appendChild(newDiv)
+
+}
+
+
 function renderCats(){
     fetch("https://api.thecatapi.com/v1/images/search")
         .then(res => res.json())
         .then(data => {
             imageLink =data[0].url;
-            console.log(data[0].url)
+            imgForLike= data[0].url;
             forms.innerHTML = `
             <div class="container">
             <div id="catPic" class="cats">
-              
-            </div>
             <img src = "${data[0].url}"/>
+            
+            </div>
+            
             `
 
         })
@@ -142,9 +181,6 @@ button.addEventListener("click",(e)=>{
     console.log(e)
 })
 
-let btnInner = `
-
-<button class="btn" id="btn"> Play Again </button>`
 
 let formsInnertext = `
 <form id="theBtn">
@@ -154,3 +190,7 @@ let formsInnertext = `
 
         </form>
         `
+
+
+            
+        });
